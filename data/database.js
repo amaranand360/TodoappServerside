6ea6f10b428e7d100c5193bc3f3eee8657060748
Amar kumar <122713145+amaranand360@users.noreply.github.com>
2023-10-05 12:27:12 +0530

    Update database.js url

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect(process.env.MONGO_URL, {
       dbName: "Tododatabase",
     })
     .then((c) => console.log(`Database Connected with ${c.connection.host}`))
